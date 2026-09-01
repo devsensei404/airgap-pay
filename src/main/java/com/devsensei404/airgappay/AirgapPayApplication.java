fbe8211c4fee843c2bd12856ccc3677254a9b3ca
devsensei404 <subhadeeppaul51@gmail.com>
2026-09-01 05:08:44 +0000

    Initial commit

---
+++ src/main/java/com/devsensei404/airgappay/AirgapPayApplication.java
@@ -0,0 +1,13 @@
+package com.devsensei404.airgappay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirgapPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirgapPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devsensei404/airgappay/AirgapPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsensei404.airgappay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirgapPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
