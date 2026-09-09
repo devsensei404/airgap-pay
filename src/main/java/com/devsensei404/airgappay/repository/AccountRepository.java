46ad15da523a5d2e3a95e7a9e57d1f54613d8d93
devsensei404 <subhadeeppaul51@gmail.com>
2026-09-09 03:08:19 +0000

    update repository files

--- src/main/java/com/devsensei404/airgappay/repository/AccountRepository.java
+++ src/main/java/com/devsensei404/airgappay/repository/AccountRepository.java
@@ -1,4 +1,10 @@
 package com.devsensei404.airgappay.repository;
 
-public interface AccountRepository {
+import com.devsensei404.airgappay.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByVpa(String vpa);
 }
--- src/main/java/com/devsensei404/airgappay/repository/TransactionRepository.java
+++ src/main/java/com/devsensei404/airgappay/repository/TransactionRepository.java
@@ -1,4 +1,8 @@
 package com.devsensei404.airgappay.repository;
 
-public interface TransactionRepository {
+import com.devsensei404.airgappay.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    boolean existsByPacketHash(String packetHash);
 }
